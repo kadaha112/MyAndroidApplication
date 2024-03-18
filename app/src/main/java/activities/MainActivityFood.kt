fd52ad5e8b49d1ca0ad6afc44cd758be0fbe0b7f
kadaha112 <kadaha112@gmail.com>
2024-03-18 17:05:25 +0900

    20240318 16:42

--- app/src/main/java/activities/MainActivityFood.kt
+++ app/src/main/java/activities/MainActivityFood.kt
@@ -1,14 +1,23 @@
 package activities
 
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.daehankang.myandroidapplication.R
+import com.daehankang.myandroidapplication.data.KakaoSearchPlaceResponse
 import com.daehankang.myandroidapplication.databinding.ActivityMainFoodBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivityFood : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainFoodBinding.inflate(layoutInflater) }
 
+    var searchQuery : String = "내주변맛집"
+
+    var myLocation : Location?= null
+    val locationProviderClient : FusedLocationProviderClient by lazy { LocationServices.getFusedLocationProviderClient(this) }
+    var searchPlaceResponse : KakaoSearchPlaceResponse? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
